f2727bc2f746c18fbc839433fedb40f32c057830
siddhivitadhekale <143020079+siddhivitadhekale@users.noreply.github.com>
2024-01-03 04:19:20 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,405 @@
+const myjokes =[`What did one hat say to the other?
+You wait here — I'll go on ahead!`,
+
+`Why don't the circus lions eat the clowns?
+Because they taste funny!`,
+
+`What did the pirate say when he turned 80?
+"Aye matey."`,
+
+`Why did the chicken cross the playground?
+To get to the other slide!`,
+
+`Why did the actor fall through the floorboards?
+They were going through a stage!`,
+
+`Why did a scarecrow win a Nobel prize?
+He was outstanding in his field!`,
+
+`Why are peppers the best at archery?
+Because they habanero!`,
+
+`What did the duck say after she bought chapstick?
+Put it on my bill!`,
+
+`Why did an old man fall in a well?
+Because he couldn’t see that well!`,
+
+`What do you call a fake noodle?
+An impasta!`,
+
+`What did the three-legged dog say when he walked into a saloon?
+“I’m looking for the man who shot my paw!”`,
+
+`How do you tell the difference between a bull and a cow?
+It is either one or the udder!`,
+
+`What’s red and smells like blue paint?
+Red paint!`,
+
+`What’s the difference between a hippo and a Zippo?
+One is very heavy, the other is a little lighter!`,
+
+`What happened when Bluebeard fell overboard in the Red Sea?
+He got marooned!`,
+
+`Why couldn't the skeleton go to school?
+His heart just wasn’t in it.`,
+
+`What did the termite say when it walked into a bar?
+"Where's the bar tender?"`,
+
+`Why can’t you send a duck to space?
+Because the bill would be astronomical!`,
+
+`What does Jeff Bezos do before he goes to sleep?
+He puts his PJ-Amazon!`,
+
+`What happened when the world's tongue-twister champion got arrested?
+They gave him a tough sentence!`,
+
+`What did the mama cow say to the calf?
+It’s pasture bedtime!`,
+
+`How does a vampire start a letter?
+Tomb it may concern!`,
+`What do you call an illegally parked frog?
+Toad!`,
+
+`What did one plate say to the other?
+Dinner is on me!`,
+
+`Why do hummingbirds hum?
+Because they don’t know the words!`,
+
+`What do sprinters eat before a race?
+Nothing. They fast!`,
+
+
+`Animal Jokes for Kids
+best jokes for kids
+What kind of cat likes living in water?
+An octo-puss.`,
+
+`Why did the dinosaur cross the road?
+Because chickens hadn't evolved yet.`,
+
+`What do you call a cow that can't make milk?
+An udder failure — a milk dud.`,
+
+`What do cats eat for breakfast?
+Mice Crispies!`,
+
+`What do you call an elephant that doesn’t matter?
+An irrelephant!`,
+
+`What do you get when you cross a rabbit with shellfish?
+An oyster bunny!`,
+
+`Where do polar bears keep their money?
+In a snow bank!`,
+
+`Why did the pony get sent to his room?
+He wouldn’t stop horsing around!`,
+
+`What kind of dog does a magician have?
+A Labracadabrador!`,
+
+`Where do cows go on Friday nights?
+They go to the moo-vies!`,
+
+`Why couldn't the pony sing “Happy Birthday?”
+Because she was just a little hoarse!`,
+
+`How do you make an octopus laugh?
+With ten-tickles!`,
+
+`How do you keep a bull from charging?
+Take away its credit card!`,
+
+`Why can’t a leopard hide?
+Because he’s always spotted!`,
+
+`How do you get a squirrel to like you?
+Act like a nut!`,
+
+`What is the difference between a cat that took a nap on the Xerox machine and a cat that imitates everything you do?
+One is a cat copy, and the other is a copy cat.`,
+
+`How do young bees get to school?
+They take the school buzz!`,
+
+`What do frogs order at fast-food restaurants?
+French flies!`,
+
+`What do you get from a pampered cow?
+Spoiled milk!`,
+
+`What do you call an alligator who solves mysteries?
+An investigator!`,
+
+`Why is a snake difficult to fool?
+You can’t pull its leg!`,
+
+`What kind of socks do grizzlies wear?
+None, they have bear feet!`,
+
+`What do you get when you cross a snail with a porcupine?
+A slowpoke!`,
+
+`What did the dog say when it sat on sandpaper?
+"Ruff!"`,
+
+`What's a cat's favorite dessert?
+Chocolate mouse!`,
+
+`What fish only swims at night?
+Starfish!`,
+
+`What does a triceratops sit on?
+Its tricera-bottom!`,
+
+`Knock Knock Jokes for Kids
+jokes for kids
+Knock, knock.
+Who’s there?
+Weirdo.
+Weirdo who?
+Weirdo you think you’re going?`,
+
+`Knock, knock.
+Who’s there?
+Howl.
+Howl who?
+Howl you know unless you open the door?`,
+
+`Knock, knock.
+Who’s there?
+Venice.
+Venice who?
+Venice your family coming home?`,
+
+`Knock, knock.
+Who’s there?
+Weekend.
+Weekend who?
+Weekend do anything we want!`,
+
+`Knock, knock.
+Who’s there?
+Figs.
+Figs who?
+Figs the doorbell, I’ve been knocking forever!`,
+
+`Knock, knock.
+Who’s there?
+Theodore.
+Theodore who?
+Theodore wasn’t opened so I knocked!`,
+
+`Knock, knock.
+Who’s there?
+Tank.
+Tank who?
+You’re welcome!`,
+
+`Knock, knock.
+Who’s there?
+Isabel.
+Isabel who?
+Isabel not working?`,
+
+`Knock, knock.
+Who’s there?
+Ice cream.
+Ice cream who?
+ICE CREAM SO YOU CAN HEAR ME!`,
+
+`Knock, knock.
+Who’s there?
+Icy.
+Icy who?
+Icy you in there!`,
+
+`Knock, knock.
+Who’s there?
+Dozen.
+Dozen who?
+Dozen anyone want to let me in?`,
+
+`Knock, knock.
+Who’s there?
+Scold.
+Scold who?
+Scold outside, let me in!`,
+
+`Knock, knock.
+Who’s there?
+Water.
+Water who?
+Water you asking so many questions for, just open up!`,
+
+`Knock, knock.
+Who’s there?
+Cargo.
+Cargo who?
+Car go, “Toot toot, vroom, vroom!”`,
+
+`Knock, knock.
+Who there?
+Noah.
+Noah who?
+Know a place I can spend the night?`,
+
+`Knock, knock.
+Who's there?
+Onion.
+Onion who?
+Onion mark, get set, go!`,
+
+`Knock, knock.
+Who's there?
+Cereal.
+Cereal who?
+Cereal pleasure to be invited over!`,
+
+`Knock, knock.
+Who’s there?
+Hatch.
+Hatch who?
+Bless you!`,
+
+`Knock, knock.
+Who’s there?
+Annie.
+Annie who?
+Annie body home?`,
+
+`Knock, knock.
+Who’s there?
+Boo.
+Boo who?
+Don’t cry, it’s just a joke!`,
+
+`Knock, knock.
+Who’s there?
+Lettuce.
+Lettuce who?
+Lettuce in, it’s cold out here!`,
+
+`Knock, knock.
+Who’s there?
+An interrupting cow.
+An interrupt—
+MOO!`,
+
+`Knock, knock.
+Who’s there?
+Owls go.
+Owls go who?
+That’s right!`,
+
+`Knock, knock.
+Who’s there?
+A little old lady.
+A little old lady who?
+Hey, I didn’t know you could yodel!`,
+
+`Knock, knock.
+Who’s there?
+Nana.
+Nana who?
+Nana your business!`,
+
+`Knock, knock.
+Who’s there?
+Toodle
+Toodle who?
+Toodle-loo!`,
+
+`Knock, knock.
+Who’s there?
+Spell.
+Spell who?
+Okay, W-H-O!`,
+
+`Food Jokes for Kids
+best jokes for kids
+How does a cucumber become a pickle?
+It goes through a jarring experience.`,
+
+`What's a turnip's favorite soda?
+Root beer!`,
+
+`What happened when the cheese shop exploded?
+Da brie was everywhere!`,
+
+`What’s a piece of bread’s least favorite chore?
+Doing a loaf of laundry.`,
+
+`What did the bunny say to the carrot?
+It’s been nice gnawing you!`,
+
+`What do you call a sad strawberry?
+A blueberry!`,
+
+`Why did the tomato blush?
+Because it saw the salad dressing!`,
+
+`What did one dried fruit say when another asked it to the movies?
+It's a date`,
+
+`Why do bees have sticky hair?
+They use honeycombs!`,
+
+`What does a cow call an earthquake?
+A milkshake!`,
+
+`Why did the man go to the yogurt museum?
+To get a little culture!`,
+
+`Why couldn't the sesame seed climb up the hill?
+Because it was on a roll!`,
+
+`Why can't you trust tacos?
+Because they always spill the beans!`,
+
+`Math Jokes for Kids
+best jokes for kids
+Why is the obtuse triangle always frustrated?
+Because it’s never right!`,
+
+`Why do plants hate math?
+Because it's full of square roots!`,
+
+`What do you call people who really like tractors?
+Protractors!`,
+
+`How come no one picks statistics as their favorite subject?
+It’s just average.`,
+
+`Why did two 4s skip dinner?
+Because they already 8!`,
+
+`Did you hear about the mathematician who’s afraid of negative numbers?
+He’ll stop at nothing to avoid them!`,
+
+`What did the triangle feel sorry for the circle?
+Because it’s pointless!`,
+
+]
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener('click',function(){
+    let randomNumber = Math.floor(Math.random()*myjokes.length);
+console.log(randomNumber)
+
+
+
+let jokes = document.getElementById("joke");
+jokes.innerHTML =myjokes[randomNumber];
+
+
+})
+
